ccf24bda1874c108161a0fc2b7a5af5f27563f47
wzbillings <39701545+wzbillings@users.noreply.github.com>
2022-03-20 00:49:24 -0400

    feat: fcn for writing bib file from renv.lock

---
+++ R/write_renv_bib.R
@@ -0,0 +1,13 @@
+#' Create a bibliography file of all renv package dependencies
+#'
+#' @param filepath The ('.bib') file to write, passed on to knitr::write_bib().
+#'
+#' @return (invisibly) a character vector of packages which was passed on to
+#'     knitr::write_bib().
+#' @export
+#'
+write_renv_bib <- function(filepath = "./pkgs.bib") {
+  renvLock <- jsonlite::read_json("renv.lock")
+  knitr::write_bib(x = names(renvLock$Packages), file = filepath)
+  invisible(names(renvLock$Packages))
+}
