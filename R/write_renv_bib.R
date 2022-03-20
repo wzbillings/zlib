08cd38e8d452ef764767b96a89795cf335634702
wzbillings <39701545+wzbillings@users.noreply.github.com>
2022-03-20 01:05:42 -0400

    fix: adds namespace checks for write_renv_bib

--- R/write_renv_bib.R
+++ R/write_renv_bib.R
@@ -1,14 +1,24 @@
 #' Create a bibliography file of all renv package dependencies
 #'
+#' See https://github.com/rstudio/renv/issues/340 for more detail
+#'
 #' @param filepath The ('.bib') file to write, passed on to knitr::write_bib().
 #'
 #' @return (invisibly) a character vector of packages which was passed on to
 #'     knitr::write_bib().
 #' @export
 #'
-#' See https://github.com/rstudio/renv/issues/340 for more details
-#'
 write_renv_bib <- function(filepath = "./pkgs.bib") {
+  # Check that jsonlite and knitr are both installed!
+  if (!requireNamepsace("jsonlite", quietly = TRUE)) {
+    stop("ℹ You do not have the package 'jsonlite'.\n",
+         "✖ zlib::write_renv_bib() will throw an error without this pkg.")
+  }
+  if (!requireNamepsace("knitr", quietly = TRUE)) {
+    stop("ℹ You do not have the package 'knitr'.\n",
+         "✖ zlib::write_renv_bib() will throw an error without this pkg.")
+  }
+
   renvLock <- jsonlite::read_json("renv.lock")
   knitr::write_bib(x = names(renvLock$Packages), file = filepath)
   invisible(names(renvLock$Packages))
