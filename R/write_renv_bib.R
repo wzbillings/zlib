8b0e83d22a38a54343a8183c73ba923cac5e0e74
wzbillings <39701545+wzbillings@users.noreply.github.com>
2022-03-20 01:13:54 -0400

    fixs link and undefined global

--- R/write_renv_bib.R
+++ R/write_renv_bib.R
@@ -1,6 +1,6 @@
 #' Create a bibliography file of all renv package dependencies
 #'
-#' See \link{https://github.com/rstudio/renv/issues/340} for more detail
+#' See \url{https://github.com/rstudio/renv/issues/340} for more detail
 #'
 #' @param filepath The ('.bib') file to write, passed on to knitr::write_bib().
 #'
@@ -10,11 +10,11 @@
 #'
 write_renv_bib <- function(filepath = "./pkgs.bib") {
   # Check that jsonlite and knitr are both installed!
-  if (!requireNamepsace("jsonlite", quietly = TRUE)) {
+  if (!utils::requireNamepsace("jsonlite", quietly = TRUE)) {
     stop("ℹ You do not have the package 'jsonlite'.\n",
          "✖ zlib::write_renv_bib() will throw an error without this pkg.")
   }
-  if (!requireNamepsace("knitr", quietly = TRUE)) {
+  if (!utils::requireNamepsace("knitr", quietly = TRUE)) {
     stop("ℹ You do not have the package 'knitr'.\n",
          "✖ zlib::write_renv_bib() will throw an error without this pkg.")
   }
