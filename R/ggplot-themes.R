32b173ea506acc66e5d11231d8d9e118ad1c0951
Zane Billings <39701545+wzbillings@users.noreply.github.com>
2023-10-25 21:55:12 -0400

    feat: updates theme_ms()

--- R/ggplot-themes.R
+++ R/ggplot-themes.R
@@ -1,12 +1,23 @@
 #' Basic plot theme for manuscript
 #'
 #' @param base_size base font size
-#' @param strip_text_size size of text that goes over facets
-#' @param axis_title_size size of axis label text
-#' @param axis_text_size size of axis tick mark text
-#' @param title_text_size size of plot title text
-#' @param legend_text_size size of text in legend
-#' @param font_family font to use in plot text
+#' @param text_size_strip size of text that goes over facets
+#' @param text_size_axis_text size of axis tick text
+#' @param text_size_axis_title size of axis label/title text
+#' @param text_size_title size of plot title text
+#' @param text_size_subtitle size of plot subtitle text
+#' @param text_size_legend size of legend text
+#' @param text_size_legend_title size of legend title text
+#' @param text_size_caption size of plot caption text
+#' @param font_family font to use in plot text, defaults to ggplot's base font
+#' @param legend_position where the legend should go, bottom by default.
+#' accepts any argument allowed by ggplot2.
+#' @param panel_spacing_lines Amount of space in line units between faceted
+#' panels.
+#' @param margin_t Margin space above top of plot.
+#' @param margin_r Margin space to the right of plot.
+#' @param margin_b Margin space underneath the of plot.
+#' @param margin_l Margin space to the left of plot.
 #' @param ... Other arguments passed to \code{theme_minimal}.
 #'
 #' @examples
@@ -18,52 +29,98 @@
 #'     theme_ms()
 #' }
 #'
+#' \dontrun{
+#' ggplot2::theme_set(zlib::theme_ms())
+#' }
+#'
 #' @export
 theme_ms <- function(base_size = 12,
-                     strip_text_size = 14,
-                     axis_title_size = 14,
-                     axis_text_size = 12,
-                     title_text_size = 16,
-                     legend_text_size = 14,
-                     font_family = "",
-                     ...) {
-  ret <- ggplot2::theme_minimal(
-    base_family = font_family,
-    base_size = base_size,
-    ...
-  )
+										 text_size_strip = 16,
+										 text_size_axis_text = 12,
+										 text_size_axis_title = 18,
+										 text_size_title = 20,
+										 text_size_subtitle = 16,
+										 text_size_legend = 16,
+										 text_size_legend_title = 18,
+										 text_size_caption = 14,
+										 font_family = "",
+										 legend_position = "bottom",
+										 panel_spacing_lines = 2,
+										 margin_t = 6,
+										 margin_r = 6,
+										 margin_b = 6,
+										 margin_l = 6,
+										 ...) {
+	# Use theme_minimal() as a default
+	ret <- ggplot2::theme_minimal(
+		base_family = font_family,
+		base_size = base_size,
+		...
+	)
 
-  ret$strip.text <- ggplot2::element_text(
-    size = strip_text_size,
-    color = "black",
-    face = "bold"
-  )
+	# WHITE background to prevent the annoying darkmode thingy
+	ret$plot.background <- ggplot2::element_rect(
+		fill = "white", color = "white"
+	)
 
-  ret$axis.title <- ggplot2::element_text(
-    size = axis_title_size,
-    color = "black"
-  )
+	ret$axis.text <- ggplot2::element_text(
+		size = text_size_axis_text,
+		color = "black"
+	)
 
-  ret$axis.text <- ggplot2::element_text(
-    size = axis_text_size,
-    color = "black"
-  )
+	ret$axis.title <- ggplot2::element_text(
+		size = text_size_axis_title,
+		color = "black"
+	)
 
-  ret$ plot.title <- ggplot2::element_text(
-    hjust = 0.5,
-    size = title_text_size,
-    face = "bold",
-    color = "black"
-  )
+	ret$strip.text <- ggplot2::element_text(
+		size = text_size_strip,
+		color = "black",
+		face = "bold"
+	)
 
-  ret$legend.position <- "bottom"
+	ret$plot.subtitle <- ggplot2::element_text(
+		size = text_size_subtitle,
+		hjust = 0,
+		margin = ggplot2::margin(b = 2)
+	)
 
-  ret$legend.justification <- "center"
+	ret$plot.title <- ggplot2::element_text(
+		size = text_size_title,
+		hjust = 0,
+		margin = ggplot2::margin(b = 2, l = 2)
+	)
 
-  ret$legend.text <- ggplot2::element_text(
-    size = legend_text_size,
-    color = "black"
-  )
+	ret$plot.caption <- ggplot2::element_text(
+		size = text_size_caption
+	)
 
-  return(ret)
+	ret$strip.text = ggplot2::element_text(
+		size = 16, hjust = 0.5, margin = ggplot2::margin(b = 2, t = 2)
+	)
+
+	ret$panel.spacing <- ggplot2::unit(panel_spacing_lines, "lines")
+
+	ret$legend.text <- ggplot2::element_text(
+		size = 16,
+		color = "black"
+	)
+	ret$legend.title = ggplot2::element_text(
+		size = text_size_legend_title,
+		color = "black"
+	)
+
+	ret$legend.position <- legend_position
+
+	ret$legend.justification <- "center"
+
+	ret$plot.margin <- ggplot2::margin(
+		t = margin_t,
+		r = margin_r,
+		b = margin_b,
+		l = margin_l
+	)
+
+	return(ret)
 }
+
